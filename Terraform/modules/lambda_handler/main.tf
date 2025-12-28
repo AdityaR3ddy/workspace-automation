3e341fdd65761953615d09112fb13bb8af67868a
Juez <adityareddy662@gmail.com>
2025-12-28 21:05:19 +0530

    correcting payload directory

--- Terraform/modules/lambda_handler/main.tf
+++ Terraform/modules/lambda_handler/main.tf
@@ -24,11 +24,11 @@ resource "aws_iam_role_policy_attachment" "lambda_logs" {
 resource "aws_lambda_function" "workspace_lambda" {
   # The zip is created in the ROOT. 
   # From modules/lambda_handler, we go up 3 levels to reach the root.
-  filename         = "${path.module}/../../../payload.zip"
+  filename         = "${path.module}/../../../lambda_folder/payload.zip"
   function_name    = "workspace_automation_handler"
   role             = aws_iam_role.iam_for_lambda.arn
   handler          = "lambda_function.lambda_handler" # filename.function
   runtime          = "python3.11"
 
-  source_code_hash = filebase64sha256("${path.module}/../../../payload.zip")
+  source_code_hash = filebase64sha256("${path.module}/../../../lambda_folder/payload.zip")
 }
