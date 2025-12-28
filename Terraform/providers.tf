b8c43a57d40f9cd8fd50523a11b2d12a82921159
Juez <adityareddy662@gmail.com>
2025-12-28 22:19:47 +0530

    updating providers.tf

--- Terraform/providers.tf
+++ Terraform/providers.tf
@@ -10,3 +10,20 @@ terraform {
 provider "aws" {
   region = "us-east-1"
 }
+
+terraform {
+  cloud {
+    organization = "TestingOrg01" # Change this to your actual TFC Org name
+
+    workspaces {
+      name = "workspace-automation" # Change this to your actual TFC Workspace name
+    }
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
