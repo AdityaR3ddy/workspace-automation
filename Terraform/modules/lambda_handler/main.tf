1cdab29e2cfecdb7b8812d53e4f9f6a99aeb40a0
Juez <adityareddy662@gmail.com>
2025-12-28 21:18:18 +0530

    last try for today :()

--- Terraform/modules/lambda_handler/main.tf
+++ Terraform/modules/lambda_handler/main.tf
@@ -22,13 +22,13 @@ resource "aws_iam_role_policy_attachment" "lambda_logs" {
 
 # The Lambda Function definition
 resource "aws_lambda_function" "workspace_lambda" {
-  # The zip is created in the ROOT. 
-  # From modules/lambda_handler, we go up 3 levels to reach the root.
-  filename         = "${path.module}/../../payload.zip"
+  # Zip is now inside the same folder as this .tf file
+  filename         = "${path.module}/payload.zip"
   function_name    = "workspace_automation_handler"
   role             = aws_iam_role.iam_for_lambda.arn
-  handler          = "lambda_function.lambda_handler" # filename.function
+  handler          = "lambda_function.lambda_handler"
   runtime          = "python3.11"
 
-  source_code_hash = filebase64sha256("${path.module}/../../payload.zip")
+  # Simplified path
+  source_code_hash = filebase64sha256("${path.module}/payload.zip")
 }
