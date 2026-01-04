4d68d9e1b2a0957855187133afb8c08145edce20
Juez <adityareddy662@gmail.com>
2026-01-04 18:17:34 +0530

    deleting

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,4 +1,4 @@
-
+/*
 #this will generate a random suffix for the s3 bucket making it unique
 resource "random_id" "suffix" {
   byte_length = 4
@@ -26,3 +26,5 @@ module "lambda_handler" {
   source = "./modules/lambda_handler"
   dynamodb_table_arn = module.dynamodb_mock_data.table_arn
 }
+
+*/
