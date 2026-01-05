2f11a12a2d427ac0187856d10682951cda299b51
Juez <adityareddy662@gmail.com>
2026-01-05 18:08:58 +0530

    adding lambda layer

--- Terraform/modules/lambda_handler/main.tf
+++ Terraform/modules/lambda_handler/main.tf
@@ -104,7 +104,6 @@ resource "aws_lambda_function" "workspace_lambda" {
   role             = aws_iam_role.iam_for_lambda.arn
   handler          = "lambda_function.lambda_handler"
   runtime          = "python3.11"
-  layers = [aws_lambda_layer_version.github_dependencies.arn]
   source_code_hash = data.archive_file.lambda_zip.output_base64sha256
   
   # Attach the layer
