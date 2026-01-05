0ecbbcb6c28c92c6fb1b3e63ddb49e6f76fa0336
Juez <adityareddy662@gmail.com>
2026-01-05 18:02:40 +0530

    adding lambda layer

--- Terraform/modules/lambda_handler/main.tf
+++ Terraform/modules/lambda_handler/main.tf
@@ -1,11 +1,25 @@
-# 1. Automatic Zip creation
+#########################################
+# 1. ARCHIVE CREATION (CODE & LAYER)
+#########################################
+
+# Zip for the Lambda Code
 data "archive_file" "lambda_zip" {
   type        = "zip"
   source_dir  = "${path.module}/../../../lambda_folder"
   output_path = "${path.module}/payload.zip"
 }
 
-# 2. IAM Role for the Lambda
+# Zip for the GitHub Layer (Ensure libraries are in 'github_layer_source/python/')
+data "archive_file" "layer_zip" {
+  type        = "zip"
+  source_dir  = "${path.module}/../../../lambda_layer_source"
+  output_path = "${path.module}/github_layer_payload.zip"
+}
+
+#########################################
+# 2. IAM ROLE & BASE PERMISSIONS
+#########################################
+
 resource "aws_iam_role" "iam_for_lambda" {
   name = "workspace_automation_role"
 
@@ -21,53 +35,84 @@ resource "aws_iam_role" "iam_for_lambda" {
   })
 }
 
-# 3. Basic Logging Permissions
+# CloudWatch Logs
 resource "aws_iam_role_policy_attachment" "lambda_logs" {
   role       = aws_iam_role.iam_for_lambda.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
-# 4. Lambda Function Definition
-resource "aws_lambda_function" "workspace_lambda" {
-  filename         = data.archive_file.lambda_zip.output_path
-  function_name    = "workspace_automation_handler"
-  role             = aws_iam_role.iam_for_lambda.arn
-  handler          = "lambda_function.lambda_handler"
-  runtime          = "python3.11"
-  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
+#########################################
+# 3. CUSTOM POLICIES (DYNAMO & SECRETS)
+#########################################
 
-  # Adding environment variables if Python code needs the table name
-  environment {
-    variables = {
-      DYNAMODB_TABLE = "WorkspaceGovernance"
-    }
-  }
-}
-
-# 5. Policy for DynamoDB Access
+# DynamoDB Governance Policy
 resource "aws_iam_policy" "lambda_dynamodb_policy" {
   name        = "LambdaGovernanceDBAccess"
   description = "Allows Lambda to read account data for governance checks"
 
   policy = jsonencode({
     Version = "2012-10-17"
-    Statement = [
-      {
-        Effect   = "Allow"
-        Action   = [
-          "dynamodb:GetItem",
-          "dynamodb:Query",
-          "dynamodb:PutItem"
-        ]
-        # FIXED: Referencing the variable instead of a local resource
-        Resource = var.dynamodb_table_arn
-      }
-    ]
+    Statement = [{
+      Effect   = "Allow"
+      Action   = ["dynamodb:GetItem", "dynamodb:Query", "dynamodb:PutItem"]
+      Resource = var.dynamodb_table_arn
+    }]
   })
 }
 
-# 6. Attachment of DynamoDB Policy to Lambda Role
+# Secrets Manager Policy (For GitHub App Credentials)
+resource "aws_iam_policy" "lambda_secrets_policy" {
+  name        = "LambdaGitHubSecretAccess"
+  description = "Allows Lambda to read GitHub App credentials from Secrets Manager"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect   = "Allow"
+      Action   = "secretsmanager:GetSecretValue"
+      # Using the secret name from your screenshot
+      Resource = "arn:aws:secretsmanager:*:*:secret:github/app_credentials-*"
+    }]
+  })
+}
+
+# Attach Policies to Role
 resource "aws_iam_role_policy_attachment" "lambda_db_attach" {
   role       = aws_iam_role.iam_for_lambda.name
   policy_arn = aws_iam_policy.lambda_dynamodb_policy.arn
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_secrets_attach" {
+  role       = aws_iam_role.iam_for_lambda.name
+  policy_arn = aws_iam_policy.lambda_secrets_policy.arn
+}
+
+#########################################
+# 4. LAMBDA LAYER & FUNCTION
+#########################################
+
+resource "aws_lambda_layer_version" "github_dependencies" {
+  filename            = data.archive_file.layer_zip.output_path
+  layer_name          = "github_automation_layer"
+  compatible_runtimes = ["python3.11"]
+  source_code_hash    = data.archive_file.layer_zip.output_base64sha256
+}
+
+resource "aws_lambda_function" "workspace_lambda" {
+  filename         = data.archive_file.lambda_zip.output_path
+  function_name    = "workspace_automation_handler"
+  role             = aws_iam_role.iam_for_lambda.arn
+  handler          = "lambda_function.lambda_handler"
+  runtime          = "python3.11"
+  layers = [aws_lambda_layer_version.github_dependencies.arn]
+  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
+  
+  # Attach the layer
+  layers = [aws_lambda_layer_version.github_dependencies.arn]
+
+  environment {
+    variables = {
+      DYNAMODB_TABLE = "WorkspaceGovernance"
+    }
+  }
 }
